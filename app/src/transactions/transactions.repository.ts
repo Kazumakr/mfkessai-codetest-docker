6f053a37ffd6a4a7ebafe7890c89d87bee4a8831
kazumakr <k.toctago@gmail.com>
2025-05-25 19:08:05 +0900

    update: optimize transaction total calculation using raw SQL query with row locking for improved concurrency handling

--- app/src/transactions/transactions.repository.ts
+++ app/src/transactions/transactions.repository.ts
@@ -10,15 +10,27 @@ export class TransactionRepository {
   async createTransaction(createTransactionDto: CreateTransactionDto) {
     return this.prisma.$transaction(
       async (tx) => {
-        //取引金額累計を取得
-        const currentTotal = await tx.transaction.aggregate({
-          where: { userId: createTransactionDto.user_id },
-          _sum: { amount: true },
-        });
+        //取引金額累計を取得。行ロック
+        const result = await tx.$queryRaw<{ total: number }[]>`
+          SELECT SUM(amount) as total
+          FROM transactions
+          WHERE user_id = ${createTransactionDto.user_id}
+          FOR UPDATE;
+        `;
 
-        //取引金額累計にリクエスト金額を足したもの
-        const totalAmount =
-          (currentTotal._sum?.amount || 0) + createTransactionDto.amount;
+        //postgresqlの場合は集約関数とFOR UPDATEを同時利用できないので、以下のように一時的なテーブルを作成
+        // const result = await tx.$queryRaw<{ total: number }[]>`
+        // WITH tmpTransactions AS (
+        //   SELECT amount FROM transactions
+        //   WHERE user_id = ${createTransactionDto.user_id}
+        //   FOR UPDATE
+        // )
+        // SELECT SUM(t.amount) as total
+        // FROM tmpTransactions t;
+        // `;
+
+        const currentTotal = result[0].total;
+        const totalAmount = Number(currentTotal) + createTransactionDto.amount;
 
         //取引金額の上限を超えている場合はエラーを返す(main_test.goのテストでPayment_requiredを想定している)
         if (totalAmount > 1000) {
